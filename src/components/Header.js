c569a7c734d942ba25a3f269bb4573986a3f94d6
mariposa-revoloteadora <117605437+mariposa-revoloteadora@users.noreply.github.com>
2024-06-30 00:39:07 -0400

    Semantic structure, with meta tags and OGP

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+    return (
+        <header>
+            <img src="../img/logo.png" alt="Little Lemon Restaurant logo"/>
+            <Nav />
+        </header>
+    );
+};
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    {/* <a href="/menu.html" ></a> */}
+                </li>
+            </ul>
+        </nav>
+    );
+};
+export default Nav;
