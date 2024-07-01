693ced89df175d4a71872098209afe8dbfa42112
aletrdgz <ale.trdgz@gmail.com>
2024-07-01 00:00:12 -0400

    Image src fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./Nav";
+import logo from "../img/logo.png";
 
 const Header = () => {
+    // const logo = require("../img/logo.png");
     return (
         <header>
-            <img src="../img/logo.png" alt="Little Lemon Restaurant logo"/>
+            <img src={logo} alt="Little Lemon Restaurant logo"/>
             <Nav />
         </header>
     );
